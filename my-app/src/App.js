83e0a772dec126a344ef3e83c7f6cf7bc4e4890a
Saif Mohammed <saifmohammedbloger@gmail.com>
2019-04-16 23:05:27 +0530

    wip created components folder and home component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './components/home'
 class App extends Component {
   render() {
     return (
@@ -9,16 +9,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome to Personal Blog !
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         <Home/>
         </header>
       </div>
     );
